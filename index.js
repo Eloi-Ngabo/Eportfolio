e417c75ef31eaaffb79c2ca6a1ec98a7fc9671b8
Eloi-Ngabo <eloingabonziza@gmail.com>
2026-04-20 15:36:27 -0500

    best

---
+++ index.js
@@ -0,0 +1,60 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+   const shape = document.querySelectorAll(".shape");
+   const x = event.clientX;
+   const y = event.clientY;
+  
+   for (let i = 0; i < shape.length; ++i) {
+      const isOdd = i % 2 !== 0;
+      const boolInt = isOdd ? -1 : 1;
+      shape[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+
+   }
+}
+
+function toggleContrast() {
+   contrastToggle = !contrastToggle;
+   if (contrastToggle) {
+    document.body.classList += " dark-theme"  
+   }
+   else {
+      document.body.classList.remove("dark-theme")
+
+   }
+}
+
+ function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible";
+
+    emailjs
+    .sendForm(
+       'service_u9xxjnm',
+        'template_8l9r8bs',
+       event.target,
+       'jXI3ULthRqd2U14Dd'
+    ).then(() => {
+    loading.classList.remove("modal__overlay--visible");
+    success.classList += " modal__overlay--visible";    
+   }).catch(() => {
+    loading.classList.remove("modal__overlay--visible");
+    alert(
+      "The email service is temporarily unavailable. please contact me directly on email@email.com"
+    );
+   })
+ }
+
+ 
+ function toggleModal() {
+   if (isModalOpen) {
+      isModalOpen = false;
+      return document.body.classList.remove("modal--open");
+   }
+  isModalOpen = true;
+ document.body.classList += " modal--open";
+ }
